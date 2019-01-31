cf4407b5e4045dd57cdb2b8f6a5314601fb6411b
weltongbi <weltongbi@gmail.com>
2019-01-31 00:01:57 -0300

    Multilangage

--- source/conf.py
+++ source/conf.py
@@ -314,3 +314,7 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+language = "pt_BR"
+locale_dirs = [ 'locale/' ]   # path is example but recommended.
+gettext_compact = False     # optional.
